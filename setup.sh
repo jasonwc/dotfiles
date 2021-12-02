7a652892d3472af521736c07d0dff092da8fb81e
Jason Carter <jason@geometer.io>
2021-12-02 11:03:54 -0800

    More from work laptop

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Setup script for setting up a new macos machine
+echo "Starting setup"
+
+# install xcode CLI
+xcode-select --install
+
+# Check for Homebrew to be present, install if it's missing
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+sudo softwareupdate --install-rosetta
